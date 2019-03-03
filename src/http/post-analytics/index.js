1360a34a38e4662170d6063aaf1863cd66e12f9c
John Otander <johnotander@gmail.com>
2019-03-03 12:18:03 -0700

    Fix timestamp bug

--- src/http/post-analytics/index.js
+++ src/http/post-analytics/index.js
@@ -2,7 +2,7 @@ const data = require('@begin/data')
 
 exports.handler = async function http(req) {
   const { domain, path } = req.body
-  const timeNow = new Date()
+  const timeNow = (new Date()).getTime()
   
   await data.set({
     table: 'pageViews',
